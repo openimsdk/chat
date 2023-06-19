779d09a3c0addb8ae1202d9a00892941459d8de6
withchao <993506633@qq.com>
2023-06-19 17:38:09 +0800

    CheckAdminOr

--- pkg/common/mctx/get.go
+++ pkg/common/mctx/get.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/errs"
 	"github.com/OpenIMSDK/chat/pkg/common/constant"
+	"github.com/OpenIMSDK/chat/pkg/common/tokenverify"
 	"strconv"
 )
 
@@ -65,4 +66,20 @@ func CheckAdminOrUser(ctx context.Context) (string, int32, error) {
 		return "", 0, err
 	}
 	return userID, userType, nil
+}
+
+func CheckAdminOr(ctx context.Context, userIDs ...string) error {
+	userID, userType, err := Check(ctx)
+	if err != nil {
+		return err
+	}
+	if userType == tokenverify.TokenAdmin {
+		return nil
+	}
+	for _, id := range userIDs {
+		if userID == id {
+			return nil
+		}
+	}
+	return errs.ErrNoPermission.Wrap("not admin or not in userIDs")
 }
