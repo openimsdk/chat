404134c9649b67ba5510c208763705d7afbd5e89
withchao <993506633@qq.com>
2023-06-30 12:14:15 +0800

    config optimization

--- pkg/sms/sms.go
+++ pkg/sms/sms.go
@@ -9,14 +9,26 @@ import (
 
 func New() (SMS, error) {
 	switch strings.ToLower(config.Config.VerifyCode.Use) {
+	case "":
+		return empty{}, nil
 	case "ali":
 		return newAli()
 	default:
-		return nil, fmt.Errorf("not support sms: %s", config.Config.VerifyCode.Use)
+		return nil, fmt.Errorf("not support sms: `%s`", config.Config.VerifyCode.Use)
 	}
 }
 
 type SMS interface {
 	Name() string
 	SendCode(ctx context.Context, areaCode string, phoneNumber string, verifyCode string) error
+}
+
+type empty struct{}
+
+func (e empty) Name() string {
+	return "empty-sms"
+}
+
+func (e empty) SendCode(ctx context.Context, areaCode string, phoneNumber string, verifyCode string) error {
+	return nil
 }
