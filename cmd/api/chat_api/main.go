0e7d465bcd7422c8fd296f86dcb073534113f180
withchao <993506633@qq.com>
2023-07-06 10:43:00 +0800

    update

--- cmd/api/chat_api/main.go
+++ cmd/api/chat_api/main.go
@@ -42,7 +42,7 @@ func main() {
 		panic(err)
 	}
 	zk, err := openKeeper.NewClient(config.Config.Zookeeper.ZkAddr, config.Config.Zookeeper.Schema,
-		openKeeper.WithFreq(time.Hour), openKeeper.WithUserNameAndPassword(config.Config.Zookeeper.UserName,
+		openKeeper.WithFreq(time.Hour), openKeeper.WithUserNameAndPassword(config.Config.Zookeeper.Username,
 			config.Config.Zookeeper.Password), openKeeper.WithRoundRobin(), openKeeper.WithTimeout(10), openKeeper.WithLogger(log.NewZkLogger()))
 	if err != nil {
 		panic(err)
