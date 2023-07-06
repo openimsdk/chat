634d5c9f3e8bfae1df9e0cca8e60a216a6147c30
withchao <993506633@qq.com>
2023-07-06 11:29:34 +0800

    update

--- pkg/common/mw/user.go
+++ pkg/common/mw/user.go
@@ -3,24 +3,18 @@ package mw
 import (
 	"context"
 	"github.com/OpenIMSDK/Open-IM-Server/pkg/common/log"
-	"github.com/OpenIMSDK/Open-IM-Server/pkg/errs"
 	"github.com/OpenIMSDK/chat/pkg/common/constant"
 	"google.golang.org/grpc"
-	"strconv"
 )
 
 func AddUserType() grpc.DialOption {
 	return grpc.WithChainUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		log.ZInfo(ctx, "add user type", "method", method)
 		if arr, _ := ctx.Value(constant.RpcOpUserType).([]string); len(arr) > 0 {
-			userType, err := strconv.Atoi(arr[0])
-			if err != nil {
-				return errs.ErrInternalServer.Wrap("user type is not int")
-			}
-			log.ZInfo(ctx, "add user type", "method", method, "userType", userType)
+			log.ZInfo(ctx, "add user type", "method", method, "userType", arr)
 			headers, _ := ctx.Value(constant.RpcCustomHeader).([]string)
 			ctx = context.WithValue(ctx, constant.RpcCustomHeader, append(headers, constant.RpcOpUserType))
-			ctx = context.WithValue(ctx, constant.RpcOpUserType, userType)
+			ctx = context.WithValue(ctx, constant.RpcOpUserType, arr)
 		}
 		return invoker(ctx, method, req, reply, cc, opts...)
 	})
