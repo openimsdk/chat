60edc4c42f0fed993a8b79c3f51263d6c16381e8
withchao <993506633@qq.com>
2023-07-28 10:52:19 +0800

    fix: call im api not carrying user_id

--- internal/api/chat.go
+++ internal/api/chat.go
@@ -22,6 +22,7 @@ import (
 	"github.com/OpenIMSDK/protocol/sdkws"
 	"github.com/OpenIMSDK/tools/checker"
 	"github.com/OpenIMSDK/tools/log"
+	"github.com/OpenIMSDK/tools/mcontext"
 	"io"
 	"net"
 	"time"
@@ -192,6 +193,9 @@ func (o *ChatApi) UpdateUserInfo(c *gin.Context) {
 	if err := checker.Validate(&req); err != nil {
 		apiresp.GinError(c, err) // 参数校验失败
 		return
+	}
+	if req.UserID == "" {
+		req.UserID = mcontext.GetOpUserID(c)
 	}
 	respUpdate, err := o.chatClient.UpdateUserInfo(c, &req)
 	if err != nil {
