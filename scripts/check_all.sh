f3a3f4f03f8c7cac1a3017bad8e4b3247597edad
Polarish <72297937+AllianceTing@users.noreply.github.com>
2023-07-31 16:00:38 +0800

    bug: Update check_all.sh

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -36,7 +36,7 @@ for i in ${service_port_name[*]}; do
     port=$(ss -tunlp| grep open_im | awk '{print $5}' | grep -w ${j} | awk -F '[:]' '{print $NF}')
     if [[ ${port} -ne ${j} ]]; then
       echo -e ${YELLOW_PREFIX}${i}${COLOR_SUFFIX}${RED_PREFIX}" service does not start normally,not initiated port is "${COLOR_SUFFIX}${YELLOW_PREFIX}${j}${COLOR_SUFFIX}
-      echo -e ${RED_PREFIX}"please check ../logs/openIM.log "${COLOR_SUFFIX}
+      echo -e ${RED_PREFIX}"please check chat/chat-current_time.log "${COLOR_SUFFIX}
       exit -1
     else
       echo -e ${j}${GREEN_PREFIX}" port has been listening,belongs service is "${i}${COLOR_SUFFIX}
