894785654d91e3852b2cc38b0288259e3d70a726
Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>
2023-08-07 15:12:55 +0800

    fix: fix scripts path
    Signed-off-by: Xinwei Xiong(cubxxw-openim) <3293172751nss@gmail.com>

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+set -o pipefail
+
 #Include shell font styles and some basic information
 SCRIPTS_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 OPENIM_ROOT=$(dirname "${SCRIPTS_ROOT}")/..
