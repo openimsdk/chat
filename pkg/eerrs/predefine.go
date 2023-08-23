ac6e9e581eb9dd24f8a29f291b30650fd4e5c351
withchao <993506633@qq.com>
2023-08-23 15:02:01 +0800

    fix: optimize error code

--- pkg/eerrs/predefine.go
+++ pkg/eerrs/predefine.go
@@ -17,20 +17,17 @@ package eerrs
 import "github.com/OpenIMSDK/tools/errs"
 
 var (
-	ErrPassword                 = errs.NewCodeError(10001, "PasswordError")
-	ErrAccountNotFound          = errs.NewCodeError(10002, "AccountNotFound")
-	ErrPhoneAlreadyRegister     = errs.NewCodeError(10003, "PhoneAlreadyRegister")
-	ErrAccountAlreadyRegister   = errs.NewCodeError(10004, "AccountAlreadyRegister")
-	ErrVerifyCodeSendFrequently = errs.NewCodeError(10005, "VerifyCodeSendFrequently") // 频繁获取验证码
-	ErrVerifyCodeNotMatch       = errs.NewCodeError(10006, "NotMatch")                 // 验证码错误
-	ErrVerifyCodeExpired        = errs.NewCodeError(10007, "expired")                  // 验证码过期
-	ErrVerifyCodeMaxCount       = errs.NewCodeError(10008, "Attempts limit")           // 验证码失败次数过多
-	ErrVerifyCodeUsed           = errs.NewCodeError(10009, "used")                     // 已经使用
-
-	ErrInvitationCodeUsed = errs.NewCodeError(10010, "InvitationCodeUsed") // 邀请码已经使用
-	ErrInvitationNotFound = errs.NewCodeError(10011, "InvitationNotFound") // 邀请码不存在
-
-	ErrForbidden = errs.NewCodeError(10012, "Forbidden")
-
-	ErrRefuseFriend = errs.NewCodeError(10013, "user refused to add as friend") // 拒绝添加好友
+	ErrPassword                 = errs.NewCodeError(20001, "PasswordError")            // 密码错误
+	ErrAccountNotFound          = errs.NewCodeError(20002, "AccountNotFound")          // 账号不存在
+	ErrPhoneAlreadyRegister     = errs.NewCodeError(20003, "PhoneAlreadyRegister")     // 手机号已经注册
+	ErrAccountAlreadyRegister   = errs.NewCodeError(20004, "AccountAlreadyRegister")   // 账号已经注册
+	ErrVerifyCodeSendFrequently = errs.NewCodeError(20005, "VerifyCodeSendFrequently") // 频繁获取验证码
+	ErrVerifyCodeNotMatch       = errs.NewCodeError(20006, "VerifyCodeNotMatch")       // 验证码错误
+	ErrVerifyCodeExpired        = errs.NewCodeError(20007, "VerifyCodeExpired")        // 验证码过期
+	ErrVerifyCodeMaxCount       = errs.NewCodeError(20008, "VerifyCodeMaxCount")       // 验证码失败次数过多
+	ErrVerifyCodeUsed           = errs.NewCodeError(20009, "VerifyCodeUsed")           // 已经使用
+	ErrInvitationCodeUsed       = errs.NewCodeError(20010, "InvitationCodeUsed")       // 邀请码已经使用
+	ErrInvitationNotFound       = errs.NewCodeError(20011, "InvitationNotFound")       // 邀请码不存在
+	ErrForbidden                = errs.NewCodeError(20012, "Forbidden")                // 限制登录注册
+	ErrRefuseFriend             = errs.NewCodeError(20013, "RefuseFriend")             // 拒绝添加好友
 )
