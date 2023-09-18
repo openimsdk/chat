76301a5dda63e1a6d1f585c7ccbe1185c5f1bc2f
cncsmonster <cncsmonster@163.com>
2023-09-18 17:26:57 +0800

    feat: make listening only after connection to components prepared

--- pkg/common/chatrpcstart/start.go
+++ pkg/common/chatrpcstart/start.go
@@ -35,11 +35,7 @@ import (
 
 func Start(rpcPort int, rpcRegisterName string, prometheusPort int, rpcFn func(client discoveryregistry.SvcDiscoveryRegistry, server *grpc.Server) error, options ...grpc.ServerOption) error {
 	fmt.Println("start", rpcRegisterName, "server, port: ", rpcPort, "prometheusPort:", prometheusPort, ", OpenIM version: ", config.Version)
-	listener, err := net.Listen("tcp", net.JoinHostPort(network.GetListenIP(config.Config.Rpc.ListenIP), strconv.Itoa(rpcPort)))
-	if err != nil {
-		return err
-	}
-	defer listener.Close()
+
 	zkClient, err := openKeeper.NewClient(config.Config.Zookeeper.ZkAddr, config.Config.Zookeeper.Schema,
 		openKeeper.WithFreq(time.Hour), openKeeper.WithUserNameAndPassword(config.Config.Zookeeper.Username,
 			config.Config.Zookeeper.Password), openKeeper.WithRoundRobin(), openKeeper.WithTimeout(10), openKeeper.WithLogger(log.NewZkLogger()))
@@ -62,5 +58,10 @@ func Start(rpcPort int, rpcRegisterName string, prometheusPort int, rpcFn func(c
 	if err != nil {
 		return utils.Wrap1(err)
 	}
+	listener, err := net.Listen("tcp", net.JoinHostPort(network.GetListenIP(config.Config.Rpc.ListenIP), strconv.Itoa(rpcPort)))
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
 	return utils.Wrap1(srv.Serve(listener))
 }
