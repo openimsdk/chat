6092be4d8bba2ab4b52fc880582bd2cec46809bb
withchao <993506633@qq.com>
2023-11-21 16:34:11 +0800

    fix: default verifyCode type and admin register user

--- pkg/common/mw/gin_log.go
+++ pkg/common/mw/gin_log.go
@@ -43,7 +43,7 @@ func GinLog() gin.HandlerFunc {
 			return
 		}
 		start := time.Now()
-		log.ZDebug(c, "gin request", "method", c.Request.Method, "uri", c.Request.RequestURI, "req header", c.Request.Header, "req body", string(req))
+		log.ZDebug(c, "gin request", "method", c.Request.Method, "uri", c.Request.RequestURI, "req", string(req))
 		c.Request.Body = io.NopCloser(bytes.NewReader(req))
 		writer := &responseWriter{
 			ResponseWriter: c.Writer,
@@ -52,6 +52,6 @@ func GinLog() gin.HandlerFunc {
 		c.Writer = writer
 		c.Next()
 		resp := writer.buf.Bytes()
-		log.ZDebug(c, "gin response", "time", time.Since(start), "status", c.Writer.Status(), "resp header", c.Writer.Header(), "resp", string(resp))
+		log.ZDebug(c, "gin response", "time", time.Since(start), "status", c.Writer.Status(), "resp", string(resp))
 	}
 }
