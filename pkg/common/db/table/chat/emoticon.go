2d8796b86e26f63b373b75e4e79ddf314777c0fd
AndrewZuo01 <andrew.zuo@wustl.edu>
2023-12-05 11:16:12 +0800

    fix bug

--- pkg/common/db/table/chat/emoticon.go
+++ pkg/common/db/table/chat/emoticon.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Emoticon struct {
-	ID       int64  `gorm:"column:id;primary_key;type:char(64)"`
+	ID       int64  `gorm:"column:id;primary_key;type:bigint"`
 	ImageURL string `gorm:"column:image_url;type:varchar(255)"`
 	OwnerID  string `gorm:"column:owner_id;type:char(64)"`
 }
