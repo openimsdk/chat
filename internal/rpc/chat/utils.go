dd771d8bd66de3cd9d403109c9d9be29ba291b92
AndrewZuo01 <andrew.zuo@wustl.edu>
2023-12-15 12:09:24 +0800

    fix bug login.go

--- internal/rpc/chat/utils.go
+++ internal/rpc/chat/utils.go
@@ -57,6 +57,7 @@ func DbToPbUserFullInfo(attribute *chat.Attribute) *common.UserFullInfo {
 		AllowBeep:        attribute.AllowBeep,
 		AllowVibration:   attribute.AllowVibration,
 		GlobalRecvMsgOpt: attribute.GlobalRecvMsgOpt,
+		EmailRegistered:  attribute.EmailRegister,
 	}
 }
 
