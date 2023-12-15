37c6d6e1470252dd250e9f214eed8dcccf18fc30
luhaoling <2198702716@qq.com>
2023-12-15 15:01:14 +0800

    fix: fix the initRedis

--- pkg/common/config/parse.go
+++ pkg/common/config/parse.go
@@ -75,66 +75,10 @@ func InitConfig(configFile string) error {
 	if err != nil {
 		return fmt.Errorf("read loacl config file error: %w", err)
 	}
+
 	if err := yaml.NewDecoder(bytes.NewReader(data)).Decode(&Config); err != nil {
 		return fmt.Errorf("parse loacl openIMConfig file error: %w", err)
 	}
-	//if Config.Envs.Discovery != "k8s" {
-	//	zk, err := openKeeper.NewClient(Config.Zookeeper.ZkAddr, Config.Zookeeper.Schema,
-	//		openKeeper.WithFreq(time.Hour), openKeeper.WithUserNameAndPassword(Config.Zookeeper.Username,
-	//			Config.Zookeeper.Password), openKeeper.WithRoundRobin(), openKeeper.WithTimeout(10), openKeeper.WithLogger(&zkLogger{}))
-	//	if err != nil {
-	//		return utils.Wrap(err, "conn zk error ")
-	//	}
-	//	defer zk.Close()
-	//	var openIMConfigData []byte
-	//	for i := 0; i < 100; i++ {
-	//		var err error
-	//		configData, err := zk.GetConfFromRegistry(constant.OpenIMCommonConfigKey)
-	//		if err != nil {
-	//			fmt.Printf("get zk config [%d] error: %v\n;envs.descoery=%s", i, err, Config.Envs.Discovery)
-	//			time.Sleep(time.Second)
-	//			continue
-	//		}
-	//		if len(configData) == 0 {
-	//			fmt.Printf("get zk config [%d] data is empty\n", i)
-	//			time.Sleep(time.Second)
-	//			continue
-	//		}
-	//		openIMConfigData = configData
-	//	}
-	//	if len(openIMConfigData) == 0 {
-	//		return errors.New("get zk config data failed")
-	//	}
-	//	if err := yaml.NewDecoder(bytes.NewReader(openIMConfigData)).Decode(&imConfig); err != nil {
-	//		return fmt.Errorf("parse zk openIMConfig: %w", err)
-	//	}
-	//	// 这里可以优化，可将其优化为结构体层面的赋值
-	//	configFieldCopy(&Config.Mysql.Address, imConfig.Mysql.Address)
-	//	configFieldCopy(&Config.Mysql.Username, imConfig.Mysql.Username)
-	//	configFieldCopy(&Config.Mysql.Password, imConfig.Mysql.Password)
-	//	configFieldCopy(&Config.Mysql.Database, imConfig.Mysql.Database)
-	//	configFieldCopy(&Config.Mysql.MaxOpenConn, imConfig.Mysql.MaxOpenConn)
-	//	configFieldCopy(&Config.Mysql.MaxIdleConn, imConfig.Mysql.MaxIdleConn)
-	//	configFieldCopy(&Config.Mysql.MaxLifeTime, imConfig.Mysql.MaxLifeTime)
-	//	configFieldCopy(&Config.Mysql.LogLevel, imConfig.Mysql.LogLevel)
-	//	configFieldCopy(&Config.Mysql.SlowThreshold, imConfig.Mysql.SlowThreshold)
-	//
-	//	configFieldCopy(&Config.Log.StorageLocation, imConfig.Log.StorageLocation)
-	//	configFieldCopy(&Config.Log.RotationTime, imConfig.Log.RotationTime)
-	//	configFieldCopy(&Config.Log.RemainRotationCount, imConfig.Log.RemainRotationCount)
-	//	configFieldCopy(&Config.Log.RemainLogLevel, imConfig.Log.RemainLogLevel)
-	//	configFieldCopy(&Config.Log.IsStdout, imConfig.Log.IsStdout)
-	//	configFieldCopy(&Config.Log.WithStack, imConfig.Log.WithStack)
-	//	configFieldCopy(&Config.Log.IsJson, imConfig.Log.IsJson)
-	//
-	//	configFieldCopy(&Config.Secret, imConfig.Secret)
-	//	configFieldCopy(&Config.TokenPolicy.Expire, imConfig.TokenPolicy.Expire)
-	//
-	//	// Redis
-	//	configFieldCopy(&Config.Redis.Address, imConfig.Redis.Address)
-	//	configFieldCopy(&Config.Redis.Password, imConfig.Redis.Password)
-	//	configFieldCopy(&Config.Redis.Username, imConfig.Redis.Username)
-	//}
 
 	configData, err := yaml.Marshal(&Config)
 	fmt.Printf("debug: %s\nconfig:\n%s\n", time.Now(), string(configData))
