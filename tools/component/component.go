4ba776847f9b3ef344766be15482e88a14034921
Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>
2023-12-19 10:52:25 +0800

    feat: set openim lint
    Signed-off-by: Xinwei Xiong(cubxxw) <3293172751nss@gmail.com>

--- tools/component/component.go
+++ tools/component/component.go
@@ -16,9 +16,10 @@ package component
 
 import (
 	"fmt"
-	"github.com/OpenIMSDK/protocol/constant"
 	"os"
 	"time"
+
+	"github.com/OpenIMSDK/protocol/constant"
 
 	"github.com/OpenIMSDK/chat/pkg/common/config"
 	"github.com/OpenIMSDK/tools/errs"
@@ -26,6 +27,10 @@ import (
 	"github.com/go-zookeeper/zk"
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
+)
+
+var (
+	MaxConnectTimes = 100
 )
 
 func initCfg(cfgPath string) error {
@@ -47,15 +52,14 @@ func ComponentCheck(cfgPath string, hide bool) error {
 		return err
 	}
 	if config.Config.Envs.Discovery != "k8s" {
-		var zkConn *zk.Conn
-		if zkConn, err = checkNewZkClient(hide); err != nil {
+		if _, err := checkNewZkClient(hide); err != nil {
 			errorPrint(fmt.Sprintf("%v.Please check if your openIM server has started", err.Error()), hide)
 			return err
 		}
-		if err = checkGetCfg(zkConn, hide); err != nil {
-			errorPrint(fmt.Sprintf("%v.Please check if your openIM server has started", err.Error()), hide)
-			return err
-		}
+		// if err = checkGetCfg(zkConn, hide); err != nil {
+		// 	errorPrint(fmt.Sprintf("%v.Please check if your openIM server has started", err.Error()), hide)
+		// 	return err
+		// }
 	}
 
 	return nil
@@ -76,7 +80,9 @@ func successPrint(s string, hide bool) {
 func newZkClient() (*zk.Conn, error) {
 	var c *zk.Conn
 	c, _, err := zk.Connect(config.Config.Zookeeper.ZkAddr, time.Second, zk.WithLogger(log.NewZkLogger()))
+	fmt.Println("zk addr=", config.Config.Zookeeper.ZkAddr)
 	if err != nil {
+		fmt.Println("zookeeper connect error:", err)
 		return nil, errs.Wrap(err)
 	} else {
 		if config.Config.Zookeeper.Username != "" && config.Config.Zookeeper.Password != "" {
@@ -89,7 +95,7 @@ func newZkClient() (*zk.Conn, error) {
 }
 
 func checkNewZkClient(hide bool) (*zk.Conn, error) {
-	for i := 0; i < 100; i++ {
+	for i := 0; i < MaxConnectTimes; i++ {
 		if i != 0 {
 			time.Sleep(3 * time.Second)
 		}
@@ -108,13 +114,15 @@ func checkNewZkClient(hide bool) (*zk.Conn, error) {
 }
 
 func checkGetCfg(conn *zk.Conn, hide bool) error {
-	for i := 0; i < 100; i++ {
+	for i := 0; i < MaxConnectTimes; i++ {
 		if i != 0 {
 			time.Sleep(3 * time.Second)
 		}
 		path := "/" + config.Config.Zookeeper.Schema + "/" + constant.OpenIMCommonConfigKey
+
 		zkConfig, _, err := conn.Get(path)
 		if err != nil {
+			fmt.Println("path =", path, "zkConfig is:", zkConfig)
 			errorPrint(fmt.Sprintf("! get zk config [%d] error: %v\n", i, err), hide)
 			continue
 		} else if len(zkConfig) == 0 {
