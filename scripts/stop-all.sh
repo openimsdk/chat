8f8fa61a1bce5617e96d4bdca57f146c5cde96c5
skiffer-git <44203734@qq.com>
2024-03-05 11:59:23 +0800

    Before starting the script, first check if the program has exited

--- scripts/stop-all.sh
+++ scripts/stop-all.sh
@@ -49,7 +49,7 @@ done
 
 if $all_services_stopped; then
     # Print "All services stopped" in green to indicate success
-    echo -e "\033[0;32mAll services stopped\033[0m"
+    echo -e "\033[0;32m All chat services stopped\033[0m"
 else
     # Print error message indicating not all services are stopped
     echo -e "\033[0;31mError: Not all services have been stopped.\033[0m"
