a76f5c8caa259be7b96bb451b4b44582bbdb779b
skiffer-git <44203734@qq.com>
2024-03-06 11:27:34 +0800

    Optimize the script

--- scripts/util.sh
+++ scripts/util.sh
@@ -88,7 +88,7 @@ check_services_with_port() {
 
 # Function to print text in yellow
 print_blue() {
-    echo -e "\033[1;33m$1\033[0m"
+    echo -e "\033[0;36m$1\033[0m"
 }
 
 # Function to print text in green
