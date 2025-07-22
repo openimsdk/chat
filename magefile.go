1df9f9711ddc0f7b1600d2371243e0e3ee15a167
Monet Lee <monet_lee@163.com>
2025-07-22 15:41:03 +0800

    refactor: support args and config using mage in pre-release-v1.8.5 (#688)

--- magefile.go
+++ magefile.go
@@ -4,24 +4,55 @@
 package main
 
 import (
-	"github.com/openimsdk/gomake/mageutil"
+	"flag"
 	"os"
-	"strings"
+
+	"github.com/openimsdk/gomake/mageutil"
 )
 
 var Default = Build
 
+var Aliases = map[string]any{
+	"buildcc": BuildWithCustomConfig,
+	"startcc": StartWithCustomConfig,
+}
+
+var (
+	customRootDir   = "."        // workDir in mage, default is "./"(project root directory)
+	customSrcDir    = "cmd"      // source code directory, default is "cmd"
+	customOutputDir = "_output1" // output directory, default is "_output"
+	customConfigDir = "config"   // configuration directory, default is "config"
+	customToolsDir  = "tools"    // tools source code directory, default is "tools"
+)
+
+// Build support specifical binary build.
+//
+// Example: `mage build chat-api chat-rpc check-component`
 func Build() {
-	platforms := os.Getenv("PLATFORMS")
-	if platforms == "" {
-		platforms = mageutil.DetectPlatform()
+	flag.Parse()
+	bin := flag.Args()
+	if len(bin) != 0 {
+		bin = bin[1:]
 	}
 
-	for _, platform := range strings.Split(platforms, " ") {
-		mageutil.CompileForPlatform(platform)
+	mageutil.Build(bin, nil)
+}
+
+func BuildWithCustomConfig() {
+	flag.Parse()
+	bin := flag.Args()
+	if len(bin) != 0 {
+		bin = bin[1:]
 	}
 
-	mageutil.PrintGreen("All binaries under cmd and tools were successfully compiled.")
+	config := &mageutil.PathOptions{
+		RootDir:   &customRootDir,
+		OutputDir: &customOutputDir,
+		SrcDir:    &customSrcDir,
+		ToolsDir:  &customToolsDir,
+	}
+
+	mageutil.Build(bin, config)
 }
 
 func Start() {
@@ -31,7 +62,37 @@ func Start() {
 		mageutil.PrintRed("setMaxOpenFiles failed " + err.Error())
 		os.Exit(1)
 	}
-	mageutil.StartToolsAndServices()
+
+	flag.Parse()
+	bin := flag.Args()
+	if len(bin) != 0 {
+		bin = bin[1:]
+	}
+
+	mageutil.StartToolsAndServices(bin, nil)
+}
+
+func StartWithCustomConfig() {
+	mageutil.InitForSSC()
+	err := setMaxOpenFiles()
+	if err != nil {
+		mageutil.PrintRed("setMaxOpenFiles failed " + err.Error())
+		os.Exit(1)
+	}
+
+	flag.Parse()
+	bin := flag.Args()
+	if len(bin) != 0 {
+		bin = bin[1:]
+	}
+
+	config := &mageutil.PathOptions{
+		RootDir:   &customRootDir,
+		OutputDir: &customOutputDir,
+		ConfigDir: &customConfigDir,
+	}
+
+	mageutil.StartToolsAndServices(bin, config)
 }
 
 func Stop() {
